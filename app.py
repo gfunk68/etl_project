b5ab622426281003faac1a61afe175686002226f
FP Wirth <56011287+fpwirth@users.noreply.github.com>
2020-01-11 11:58:16 -0600

    Add app.py change #1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, redirect
 
 #################################################
 # Database Setup
